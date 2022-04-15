94d8e0ebfb0320647bbd0b8b1e27a5df165ef8fb
Aviral Gupta <aaviralgupta@gmail.com>
2022-04-15 14:33:43 +0530

    adding CORS support for Product and Product Category

--- src/main/java/com/web/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/web/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.web.ecommerce.dao;
 
 import com.web.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
