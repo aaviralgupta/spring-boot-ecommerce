33c791e561eb20f3531c78d80e6925a635bb6e6a
Aviral Gupta <aaviralgupta@gmail.com>
2022-05-29 01:08:39 +0530

    Changed lombok annotation to avoid lombok issues

--- src/main/java/com/web/ecommerce/entity/Country.java
+++ src/main/java/com/web/ecommerce/entity/Country.java
@@ -1,13 +1,15 @@
 package com.web.ecommerce.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name="country")
-@Data
+@Getter
+@Setter
 public class Country {
 
     @Id
