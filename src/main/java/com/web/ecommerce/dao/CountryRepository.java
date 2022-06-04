265718fea4e890f79f01d10fb4a4e233679e314f
Aviral Gupta <aaviralgupta@gmail.com>
2022-06-05 00:21:47 +0530

    Removed @CrossOrigin annotation as no longer needed

--- src/main/java/com/web/ecommerce/dao/CountryRepository.java
+++ src/main/java/com/web/ecommerce/dao/CountryRepository.java
@@ -3,9 +3,7 @@ package com.web.ecommerce.dao;
 import com.web.ecommerce.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Long> {
 }
